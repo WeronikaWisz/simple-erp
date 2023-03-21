e2e6154b187f6b63db3567ff0bfb055e0d66805c
Weronika Wisz <wisz.weronika@gmail.com>
2023-03-21 17:39:47 +0100

    initial commit

---
+++ simple-erp-app/src/test/java/com/simpleerp/simpleerpapp/SimpleErpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleerp.simpleerpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleErpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
